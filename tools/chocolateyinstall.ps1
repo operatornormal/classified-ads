1a2ba98427a3b06fa88a6bf95003da4e57a8f24a
Antti Järvinen <antti.jarvinen@katiska.org>
2017-11-21 23:10:46 +0200

    Version bump to 0.12.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 $packageName = 'classified-ads' 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://github.com/operatornormal/classified-ads/raw/df67d1b7dfff90916e3e1e527bbef404c806b290/Classified-ads-Win32.exe' # download url
+$url = 'https://github.com/operatornormal/classified-ads/raw/8fa7328df30a54203313f045e50fe7ea5375e99a/Classified-ads-Win32.exe' # download url
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,7 +15,7 @@ $packageArgs = @{
   silentArgs    = "/S" 
   validExitCodes= @(0, 3010, 1641)
   registryUninstallerKey = 'classified-ads' #ensure this is the value in the registry
-  checksum      = '75963c8923e8c8949f217dee17cbc393a5700eb1'
+  checksum      = 'ab0e44d0899a089b54d988f18c02829935535263'
   checksumType  = 'sha1' #default is md5, can also be sha1
 }
 
