61d049a7695dfcb8c07fd9e6f350c31d14444538
Antti Järvinen <antti.jarvinen@katiska.org>
2023-03-12 19:13:23 +0200

    Version number bump inside source code -> 0.16.
    Signed-off-by: Antti Järvinen <antti.jarvinen@katiska.org>

--- mcontroller.h
+++ mcontroller.h
@@ -1,5 +1,5 @@
 /*     -*-C++-*- -*-coding: utf-8-unix;-*-
-  Classified Ads is Copyright (c) Antti Järvinen 2013-2021.
+  Classified Ads is Copyright (c) Antti Järvinen 2013-2023.
 
   This file is part of Classified Ads.
 
@@ -25,7 +25,7 @@
 #include "datamodel/netrequestexecutor.h"
 #include "datamodel/cadbrecord.h" // for CaDbRecord::SearchTerms
 
-#define CLASSIFIED_ADS_VERSION "0.14"
+#define CLASSIFIED_ADS_VERSION "0.16"
 
 class Node ;
 class Model ;
